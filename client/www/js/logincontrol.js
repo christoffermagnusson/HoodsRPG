2fd0eb886259dcea5e1277d9597c592989262eec
Dunklas <dunklas@gmail.com>
2017-01-18 17:48:48 +0100

    New template

--- client/www/js/logincontrol.js
+++
@@ -1,7 +0,0 @@
-angular.module('hoods.logincontrol', ['ionic'])
-
-.controller('LoginCtrl', function($scope, $location) {
-    $scope.go = function(path) {
-        $location.path(path);
-    };
-});
