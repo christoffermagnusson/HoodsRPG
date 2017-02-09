96b86ba973767ccad829007370618ca91f054825
Ubuntu <ubuntu@ip-172-31-17-117.ec2.internal>
2017-02-09 19:50:07 +0000

    Added chat module for server, working on a problem parsing JSON correctly to client

---
+++ server/hoods/src/main/java/org/kebab/CHATMESSAGEHandler.java
@@ -0,0 +1,17 @@
+package org.kebab;
+
+
+
+public class CHATMESSAGEHandler implements IHandler{
+
+
+	public CHATMESSAGEHandler(){}
+
+		
+	@Override
+	public void handle(Message message){
+		Message response = new Message(Command.BROADCASTCHATMESSAGE, message.getParams(),message.getSender());
+		response.send();	
+	}					
+
+}
